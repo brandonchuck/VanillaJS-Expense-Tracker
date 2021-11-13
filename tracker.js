fd6eaeb72a5b6f5999d278ecded1c2c9c7b4fc28
Brandon Chuck <blc42310@gmail.com>
2021-11-12 18:56:23 -0800

    Tweaked CSS styling; Added icons for currency field; formatted amount to display ; fields now reset after form submission

--- tracker.js
+++ tracker.js
@@ -1,14 +1,15 @@
-document.querySelector('.expense-form').addEventListener('submit', createRow);
+const form = document.querySelector('.expense-form');
+form.addEventListener('submit', createRow);
 
 function createRow(e){
   e.preventDefault();
 
   // Input values
-  const currency = document.querySelector('.currency').value;
-  const description = document.querySelector('.description').value;
+  const currency = displayCurrency(document.querySelector('.currency').value);
   const date = document.querySelector('.date').value;
-  const amount = document.querySelector('.amount').value;
+  const description = document.querySelector('.description').value;
   const purchaseLocation = document.querySelector('.location').value;
+  const amount = formatAmount(document.querySelector('.amount').value);
   const removeBtn = document.createElement('button');
   
   const rowData = [currency, date, description, purchaseLocation, amount, removeBtn];
@@ -22,10 +23,14 @@ function createRow(e){
     if (i === rowData.length - 1){
       const removeButton = createRemoveButton(newRow);
       newCell.appendChild(removeButton);
+      newCell.className = 'remove-cell';
     } else {
-      newCell.textContent = rowData[i];
+      newCell.innerHTML = rowData[i];
+      newCell.className = 'data-cell';
     }
   }
+
+  form.reset();
 }
 
 function createRemoveButton(row){
@@ -40,4 +45,21 @@ function createRemoveButton(row){
 
 function removeExpense(row){
   row.remove();
+}
+
+function formatAmount(amount){
+  return `$${amount}`;
+}
+
+function displayCurrency(currency){
+  switch(currency){
+    case "Cash":
+      return currency = '<i class="bi bi-cash-coin"></i>';
+    case "Card":
+      return currency = '<i class="bi bi-credit-card-2-back"></i>';
+    case "Crypto":
+      return currency = '<i class="bi bi-currency-bitcoin"></i>';
+    case "Other":
+      return currency = 'Other';
+  }
 }
